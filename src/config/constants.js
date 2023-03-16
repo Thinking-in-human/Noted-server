225a42e47bfe1f03e589ace1f6c98ea4fb0418b4
hweejae-shin <106131005+hweejae-shin@users.noreply.github.com>
2023-03-16 20:26:56 +0900

    Feature: s3 storage setup (#3)
    * Feat. s3 setup
    s3 bucket생성, CORS설정
    
    * Fix 개행
    외부 라이브러리와 내부 모듈 개행
    
    * Feat. RESTful한 S3 API
    PDF업로드 시 s3의 Key 값을 RERSful하게 변경
    
    * Feat. RESTful한 S3 API
    PDF업로드 시 s3의 Key 값을 RERSful하게 변경
    
    ---------
    
    Co-authored-by: hweejae <hwejae25@gamil.com>

--- src/config/constants.js
+++ src/config/constants.js
@@ -3,6 +3,10 @@ require("dotenv").config();
 const CONFIG = {
   PORT: process.env.PORT,
   MONGODB: process.env.MONGODB,
+  S3_REGION: process.env.S3_REGION,
+  S3_ACCESS_KEY_ID: process.env.S3_ACCESS_KEY_ID,
+  S3_SECRET_ACCESSKEY: process.env.S3_SECRET_ACCESSKEY,
+  S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
   JWT_SECRET: process.env.JWT_SECRET,
 };
 
