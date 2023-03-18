e195cfcedbdb4101f3ed74ced4050f8791b23c22
hweejae <hwejae25@gamil.com>
2023-03-19 00:14:33 +0900

    Feat. 유저가 요청한 PDF파일을 S3에서 불러와 프론트에 전달

--- src/controllers/documents.controller.js
+++ src/controllers/documents.controller.js
@@ -1,5 +1,7 @@
 const User = require("../models/User");
 const Pdf = require("../models/Pdf");
+const { getDocumentInS3 } = require("../service/s3utils");
+const ERRORMESSAGE = require("../constants/error");
 
 exports.getAll = async (req, res, next) => {
   try {
@@ -14,9 +16,29 @@ exports.getAll = async (req, res, next) => {
       documents: userInfo.pdfDocuments,
     });
   } catch (error) {
-    error.message = "Internal Server Error";
+    error.message = ERRORMESSAGE.ERROR_500;
     error.status = 500;
 
     next(error);
+  }
+};
+
+exports.getDocument = async (req, res, next) => {
+  try {
+    const { documentTitle } = req.params;
+
+    const pdfDocument = await getDocumentInS3(documentTitle, next);
+
+    res.set({
+      "Content-Type": "application/pdf",
+      "Content-Length": pdfDocument.length,
+    });
+
+    res.send(pdfDocument);
+  } catch (error) {
+    error.message = ERRORMESSAGE.ERROR_500;
+    error.status = 500;
+
+    return next(error);
   }
 };
--- src/routes/users.js
+++ src/routes/users.js
@@ -5,5 +5,9 @@ const documentsController = require("../controllers/documents.controller");
 const router = express.Router();
 
 router.get("/:userId/documents", documentsController.getAll);
+router.get(
+  "/:userId/documents/:documentTitle",
+  documentsController.getDocument
+);
 
 module.exports = router;
